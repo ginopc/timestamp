3ba19c577c1498e79c61e7bdfc708d22c6c73103
ginopc <ginopc@tiscali.it>
2017-04-26 10:46:57 +0000

    Ok app

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
+var path = require('path');
 var PORT = process.env.PORT || 8080;
 
-app.get('/', function(req, res){
-	var result = {
-		unix: undefined,
-		natural: undefined
-	};
+// define static pages
+app.use(express.static(path.join(__dirname, 'public')));
 
-	if (req == '') {
-		console.log('Nothing in request');
-		console.log(req);
-   		res.send('Timestamps machine.');
+app.get('/:datestring', function(req, res){
+	var myDate;
+	var datestring = req.params.datestring;
+	var result = {
+		unix: null,
+		natural: null
+	}
+	
+	if (+datestring > 0) {
+		// unix timestamp
+		console.log('New Unix timestamp: ' + datestring)
+		myDate = new Date(+datestring);
 	}
 	else {
-		console.log('Return timestamp');
-		res.send(JSON.stringify(result));
+		if (moment(datestring).isValid()) {
+			console.log('New Natural timestamp: ' + datestring)
+			myDate = new Date(datestring);
+		}
+		else {
+			console.log('Invalid timestamp entered: ' + datestring);
+			myDate = null;
+		}
 	}
+	
+	if (myDate != null)
+		result = {
+			unix: moment(myDate).format('X'),
+			natural: moment(myDate).format('MMMM D, YYYY')
+		}
+
+	res.json(result);
 });
 
 app.listen( PORT, function(){
