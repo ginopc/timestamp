d6ccb4427492c43411ec8b766319e23e3b2d44da
MaurizioAru <ginopc@tiscali.it>
2017-04-26 09:48:22 +0200

    Create script basic structure

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-   res.send('Hello, world!');
+	var result = {
+		unix: undefined,
+		natural: undefined
+	};
+
+	if (req == '') {
+		console.log('Nothing in request');
+		console.log(req);
+   		res.send('Timestamps machine.');
+	}
+	else {
+		console.log('Return timestamp');
+		res.send(JSON.stringify(result));
+	}
 });
 
 app.listen( PORT, function(){
